118dc58a1f51ffd622dc2b3abe2ebcf69e727d43
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-06-22 19:09:27 +0700

    named return values

--- functions.go
+++ functions.go
@@ -6,8 +6,18 @@ func saySalam(akhi string) (string, bool) {
 	return "Assalamualaikum " + akhi, akhi != ""
 }
 
+func summa(a, b int) (total int) {
+	total = a + b
+
+	return total
+}
+
 func main() {
 	akhi1, _ := saySalam("")
 
 	fmt.Println(akhi1)
+
+	result := summa(20, 40)
+
+	fmt.Println(result)
 }
