6717df721e895cf7f336a145f0f72ff7f48f6982
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-06-22 13:01:14 +0700

    datatypes & conversion

---
+++ data-types.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var isSundanese = true
+	fmt.Println(isSundanese)
+
+	var country string = "China"
+	fmt.Println("Country = ", country)
+
+	totalUsers := 0
+	fmt.Println(totalUsers)
+
+	trxPrice := 120.90
+	fmt.Println(trxPrice)
+
+	var value32 int32 = 2221
+
+	fmt.Println(value32)
+
+	var value64 int64 = int64(value32)
+
+	fmt.Println(value64)
+}
