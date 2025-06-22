796efda47dc699928dc66fdb2d3242eba232e54d
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-06-22 15:07:56 +0700

    conditions

---
+++ conditions.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	if true {
+		fmt.Println("fr")
+	} else {
+		fmt.Println("ts pmo")
+	}
+
+	num := 0
+	if isMore := num > 0; isMore {
+		fmt.Println("ez")
+	} else {
+		fmt.Println("sybau")
+	}
+
+	name := "Agus"
+	switch name {
+	case "Agus":
+		fmt.Println("ahay")
+	default:
+		fmt.Println("kys")
+	}
+
+	switch {
+	case name == "":
+		fmt.Println("xxx")
+	default:
+		fmt.Println("sds")
+	}
+}
