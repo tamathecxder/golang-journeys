e59e6839607d639f3a7ac3b092a70ddccfc8723b
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-06-22 19:13:58 +0700

    variadic/varags

--- functions.go
+++ functions.go
@@ -12,6 +12,15 @@ func summa(a, b int) (total int) {
 	return total
 }
 
+func decreaseAll(nums ...int) int {
+	total := 0
+	for _, num := range nums {
+		total -= num
+	}
+
+	return total
+}
+
 func main() {
 	akhi1, _ := saySalam("")
 
@@ -20,4 +29,9 @@ func main() {
 	result := summa(20, 40)
 
 	fmt.Println(result)
+
+	nums := []int{20, 10, 30, 40}
+
+	fmt.Println(decreaseAll(20, 20, 50, 50))
+	fmt.Println(decreaseAll(nums...))
 }
