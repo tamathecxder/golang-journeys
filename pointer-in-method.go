38ab9d417e0fef41129a35b925a1774d9493d412
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-06-28 16:26:06 +0700

    pointer in struct method

---
+++ pointer-in-method.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Shawty struct {
+	Name string
+}
+
+func (shawty *Shawty) getShawty() {
+	var baddieNames []string = []string{
+		"Jessica", "Natalia", "Amanda",
+	}
+
+	for _, name := range baddieNames {
+		if shawty.Name == name {
+			shawty.Name = shawty.Name + " is a baddie!"
+			return
+		}
+	}
+
+	shawty.Name = shawty.Name + " is a regular girl"
+}
+
+func main() {
+	urShawty := Shawty{"Nancy"}   // default
+	urShawty2 := Shawty{"Amanda"} // pointer
+
+	fmt.Println(urShawty.Name)
+	fmt.Println(urShawty2.Name)
+
+	urShawty.getShawty()
+	urShawty2.getShawty()
+
+	fmt.Println(urShawty.Name)
+	fmt.Println(urShawty2.Name)
+}
