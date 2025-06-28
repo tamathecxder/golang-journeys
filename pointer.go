2fb5769c85e8a89dc089934227d5f70c8c2fa933
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-06-28 15:20:51 +0700

    pointer

---
+++ pointer.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	Name, Gender, Hobby string
+}
+
+func main() {
+	// ddefault: pass by value (duplicate the data)
+	// human1 := Human{"Usep Bulukumba", "Male", "Futsal"}
+	// human2 := human1
+
+	var human1 Human = Human{"Usep Bulukumba", "Male", "Futsal"}
+	var human2 *Human = &human1 // &pointer: pass by reference (pointing to the same data)
+
+	human2.Name = "Ajat Summarecon"
+
+	fmt.Println(human1)
+	fmt.Println(human1.Name)
+	fmt.Println("======")
+	fmt.Println(human2)
+	fmt.Println(human2.Name)
+}
