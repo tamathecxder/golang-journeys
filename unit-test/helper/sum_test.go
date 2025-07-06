33afed6917b7f4e95e98eb991ae5d3b9c84fdd95
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-07-06 13:14:03 +0700

    table test

--- unit-test/helper/sum_test.go
+++ unit-test/helper/sum_test.go
@@ -22,3 +22,44 @@ func TestSum(t *testing.T) {
 		fmt.Println("Sum 2 is done.")
 	})
 }
+
+type Assignment struct {
+	Name               string
+	RequestX, RequestY int
+	Expected           int
+	Description        string
+}
+
+func TestTableTest(t *testing.T) {
+	list := []Assignment{
+		{
+			Name:        "20 + 20",
+			RequestX:    20,
+			RequestY:    20,
+			Expected:    40,
+			Description: "The result must be '40'",
+		},
+		{
+
+			Name:        "100 + 90",
+			RequestX:    100,
+			RequestY:    90,
+			Expected:    190,
+			Description: "The result must be '190'",
+		},
+		{
+			Name:        "1 + 1",
+			RequestX:    1,
+			RequestY:    1,
+			Expected:    2,
+			Description: "The result must be '2'",
+		},
+	}
+
+	for _, v := range list {
+		t.Run(v.Name, func(t *testing.T) {
+			assert.Equal(t, v.Expected, Sum(v.RequestX, v.RequestY), v.Description)
+			fmt.Printf("Test with name '%s' is success.\n", v.Name)
+		})
+	}
+}
