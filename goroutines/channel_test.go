67df39b2e3513c933eeb3743b451e7c0f76692f3
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-07-13 13:01:58 +0700

    channel as params

--- goroutines/channel_test.go
+++ goroutines/channel_test.go
@@ -20,3 +20,22 @@ func TestCreateChannel(t *testing.T) {
 
 	fmt.Println("Data from channel:", data)
 }
+
+func TestChannelAsParams(t *testing.T) {
+	emailChan := make(chan string)
+
+	username := "agusgaming"
+
+	go generateEmail(username, emailChan)
+
+	emailResult := <-emailChan
+
+	fmt.Println("Email:", emailResult)
+}
+
+func generateEmail(username string, channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- fmt.Sprintf("%s@example.com", username)
+
+	fmt.Println("Successfully generate the email")
+}
