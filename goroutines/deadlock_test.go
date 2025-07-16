80bcce719428e979e4653de94593ff3a4809d9c1
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-07-16 23:36:01 +0700

    deadlock simulations

---
+++ goroutines/deadlock_test.go
@@ -0,0 +1,65 @@
+package goroutines
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+)
+
+type UserBalance struct {
+	sync.Mutex
+	Name   string
+	Amount int
+}
+
+func (u *UserBalance) Lock() {
+	u.Mutex.Lock()
+}
+
+func (u *UserBalance) Unlock() {
+	u.Mutex.Unlock()
+}
+
+func (u *UserBalance) UpdateAmount(amount int) {
+	u.Amount = u.Amount + amount
+}
+
+func Transfer(fromUser *UserBalance, toUser *UserBalance, amount int) {
+	fromUser.Lock()
+	fromUser.UpdateAmount(-amount)
+	fmt.Println("Locking User 1...")
+
+	time.Sleep(1 * time.Second)
+
+	toUser.Lock()
+	toUser.UpdateAmount(amount)
+	fmt.Println("Locking User 2...")
+
+	time.Sleep(1 * time.Second)
+
+	fromUser.Unlock()
+	toUser.Unlock()
+}
+
+func TestDeadlock(t *testing.T) {
+	asep := UserBalance{
+		Name:   "Asep",
+		Amount: 5000,
+	}
+
+	ujang := UserBalance{
+		Name:   "Ujang",
+		Amount: 5000,
+	}
+
+	go Transfer(&asep, &ujang, 1000)
+	go Transfer(&ujang, &asep, 2000)
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("Transfer process is completed")
+	fmt.Printf("%s total balance: %s\n", asep.Name, strconv.Itoa(asep.Amount))
+	fmt.Printf("%s total balance: %s\n", ujang.Name, strconv.Itoa(ujang.Amount))
+}
