0d292e3a0cb4fc150f3d84c3a4d01b0a34ce5822
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-07-17 10:42:55 +0700

    sync.Once

--- goroutines/deadlock_test.go
+++ goroutines/deadlock_test.go
@@ -83,3 +83,26 @@ func TestWaitGroup(t *testing.T) {
 
 	fmt.Println("The whole process is completed!")
 }
+
+var counter int = 0
+
+func OnlyOnce() {
+	counter++
+}
+
+func TestOnce(t *testing.T) {
+	once := sync.Once{}
+	group := sync.WaitGroup{}
+
+	for i := 1; i <= 100; i++ {
+		go func() {
+			group.Add(1)
+			once.Do(OnlyOnce)
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+
+	fmt.Println("Total counter:", counter)
+}
