a7308b304679031b7bcb99f5b031f377142b720d
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-07-27 11:11:36 +0700

    query select

--- database-mysql/database_test.go
+++ database-mysql/database_test.go
@@ -8,7 +8,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func TestExecContext(t *testing.T) {
+func TestExecSql(t *testing.T) {
 	db := SetConnection()
 	defer db.Close()
 
@@ -22,4 +22,33 @@ func TestExecContext(t *testing.T) {
 	}
 
 	fmt.Println("Successfully created customer!")
+}
+
+func TestQuerySql(t *testing.T) {
+	db := SetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := "select id, name from customers"
+	rows, err := db.QueryContext(ctx, query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+
+		err := rows.Scan(&id, &name)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("ID: ", id)
+		fmt.Println("Name: ", name)
+	}
 }
