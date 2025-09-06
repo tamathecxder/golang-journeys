9c731b2d574a67bf87df5a73012a02cd2e1b53d3
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-09-06 15:17:02 +0700

    httptest

---
+++ web/http_test.go
@@ -0,0 +1,27 @@
+package web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func DummyHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "(DUMMY_RESPONSE)")
+}
+
+func TestHttp(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	DummyHandler(recorder, req)
+
+	res := recorder.Result()
+	body := res.Body
+
+	result, _ := io.ReadAll(body)
+
+	fmt.Println(string(result))
+}
