75c51173440b45f981329230e8b8b6c6c403db2b
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-09-06 14:48:42 +0700

    http handler

--- web/server_test.go
+++ web/server_test.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -10,6 +11,23 @@ const ADDR = "localhost:8080"
 func TestServer(t *testing.T) {
 	server := http.Server{
 		Addr: ADDR,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(w, "Hello, Server!")
+	}
+
+	server := http.Server{
+		Addr:    ADDR,
+		Handler: handler,
 	}
 
 	err := server.ListenAndServe()
