927f6de25e846f1f9b6bb5147930f97ac0c14d01
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-09-06 15:11:47 +0700

    request

--- web/server_test.go
+++ web/server_test.go
@@ -59,3 +59,23 @@ func TestServeMux(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestRequest(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.Method)
+		fmt.Fprint(w, r.RequestURI)
+	})
+
+	server := http.Server{
+		Addr:    ADDR,
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
