b1e86f16f30f036e538b4c1b5bbc0ee2fb475980
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-09-06 17:52:13 +0700

    http header

---
+++ web/header_test.go
@@ -0,0 +1,28 @@
+package web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func PrintHeader(w http.ResponseWriter, r *http.Request) {
+	author := r.Header.Get("X-Author")
+	fmt.Fprint(w, author)
+}
+
+func TestHeader(t *testing.T) {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodPost, "http://localhost:8080", nil)
+
+	r.Header.Add("X-Author", "gwej")
+
+	PrintHeader(w, r)
+
+	res := w.Result()
+	body, _ := io.ReadAll(res.Body)
+
+	fmt.Println(string(body))
+}
